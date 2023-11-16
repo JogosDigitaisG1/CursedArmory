d998a88055094b448bced4e739679a6f9c9ccb13
LuiscVillarrealL <luiscvillarreall@gmail.com>
2023-11-16 23:21:08 +0000

    look at mouse while moving

--- Assets/Animations/Player/playerAnimEnum.cs
+++ Assets/Animations/Player/playerAnimEnum.cs
@@ -4,6 +4,8 @@ public static class PlayerCons
 
     public static string xMove { get; } = "xMove";
     public static string yMove { get; } = "yMove";
+
+    public static string lookAngle { get; } = "lookAngle";
     public static string walkAnim { get; } = "walkPlayer";
     public static string idleAnim { get; } = "idlePlayer";
     public static string attackAnim { get; } = "attackPlayer";
